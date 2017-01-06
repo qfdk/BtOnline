e1e0f5ae4406d3c37125ae615bddab9932600fcc
qfdk <qfdk@vip.qq.com>
2017-01-06 21:23:34 +0100

    [Update] add bootstrap style
    [Update] add bootstrap style

--- webUI/static/js/app.js
+++ webUI/static/js/app.js
@@ -1,66 +1,72 @@
-var torrentId = 'https://webtorrent.io/torrents/sintel.torrent'
+$(function () {
+  $("#go").on('click', function () {
 
-var client = new WebTorrent()
+    var torrentId = $("#btlink").val()
 
-// HTML elements
-var $body = document.body
-var $progressBar = document.querySelector('#progressBar')
-var $numPeers = document.querySelector('#numPeers')
-var $downloaded = document.querySelector('#downloaded')
-var $total = document.querySelector('#total')
-var $remaining = document.querySelector('#remaining')
-var $uploadSpeed = document.querySelector('#uploadSpeed')
-var $downloadSpeed = document.querySelector('#downloadSpeed')
+    var client = new WebTorrent()
 
-// Download the torrent
-client.add(torrentId, function (torrent) {
+    // HTML elements
+    var $body = document.body
+    var $progressBar = document.querySelector('#progressBar')
+    var $numPeers = document.querySelector('#numPeers')
+    var $downloaded = document.querySelector('#downloaded')
+    var $total = document.querySelector('#total')
+    var $remaining = document.querySelector('#remaining')
+    var $uploadSpeed = document.querySelector('#uploadSpeed')
+    var $downloadSpeed = document.querySelector('#downloadSpeed')
 
-  // Stream the file in the browser
-  torrent.files[0].appendTo('#output')
+    // Download the torrent
+    client.add(torrentId, function (torrent) {
 
-  // Trigger statistics refresh
-  torrent.on('done', onDone)
-  setInterval(onProgress, 500)
-  onProgress()
-  
-  // Statistics
-  function onProgress () {
-    // Peers
-    $numPeers.innerHTML = torrent.numPeers + (torrent.numPeers === 1 ? ' peer' : ' peers')
+      // Stream the file in the browser
+      torrent.files[0].appendTo('#output')
 
-    // Progress
-    var percent = Math.round(torrent.progress * 100 * 100) / 100
-    $progressBar.style.width = percent + '%'
-    $downloaded.innerHTML = prettyBytes(torrent.downloaded)
-    $total.innerHTML = prettyBytes(torrent.length)
+      // Trigger statistics refresh
+      torrent.on('done', onDone)
+      setInterval(onProgress, 500)
+      onProgress()
 
-    // Remaining time
-    var remaining
-    if (torrent.done) {
-      remaining = 'Done.'
-    } else {
-      remaining = moment.duration(torrent.timeRemaining / 1000, 'seconds').humanize()
-      remaining = remaining[0].toUpperCase() + remaining.substring(1) + ' remaining.'
+      // Statistics
+      function onProgress() {
+        // Peers
+        $numPeers.innerHTML = torrent.numPeers + (torrent.numPeers === 1 ? ' peer' : ' peers')
+
+        // Progress
+        var percent = Math.round(torrent.progress * 100 * 100) / 100
+        $progressBar.style.width = percent + '%'
+        $downloaded.innerHTML = prettyBytes(torrent.downloaded)
+        $total.innerHTML = prettyBytes(torrent.length)
+
+        // Remaining time
+        var remaining
+        if (torrent.done) {
+          remaining = 'Done.'
+        } else {
+          remaining = moment.duration(torrent.timeRemaining / 1000, 'seconds').humanize()
+          remaining = remaining[0].toUpperCase() + remaining.substring(1) + ' remaining.'
+        }
+        $remaining.innerHTML = remaining
+
+        // Speed rates
+        $downloadSpeed.innerHTML = prettyBytes(torrent.downloadSpeed) + '/s'
+        $uploadSpeed.innerHTML = prettyBytes(torrent.uploadSpeed) + '/s'
+      }
+      function onDone() {
+        $body.className += ' is-seed'
+        onProgress()
+      }
+    })
+
+    // Human readable bytes util
+    function prettyBytes(num) {
+      var exponent, unit, neg = num < 0, units = ['B', 'kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
+      if (neg) num = -num
+      if (num < 1) return (neg ? '-' : '') + num + ' B'
+      exponent = Math.min(Math.floor(Math.log(num) / Math.log(1000)), units.length - 1)
+      num = Number((num / Math.pow(1000, exponent)).toFixed(2))
+      unit = units[exponent]
+      return (neg ? '-' : '') + num + ' ' + unit
     }
-    $remaining.innerHTML = remaining
-    
-    // Speed rates
-    $downloadSpeed.innerHTML = prettyBytes(torrent.downloadSpeed) + '/s'
-    $uploadSpeed.innerHTML = prettyBytes(torrent.uploadSpeed) + '/s'
-  }
-  function onDone () {
-    $body.className += ' is-seed'
-    onProgress()
-  }
-})
 
-// Human readable bytes util
-function prettyBytes(num) {
-	var exponent, unit, neg = num < 0, units = ['B', 'kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
-	if (neg) num = -num
-	if (num < 1) return (neg ? '-' : '') + num + ' B'
-	exponent = Math.min(Math.floor(Math.log(num) / Math.log(1000)), units.length - 1)
-	num = Number((num / Math.pow(1000, exponent)).toFixed(2))
-	unit = units[exponent]
-	return (neg ? '-' : '') + num + ' ' + unit
-}
+  });
+});
