46d19effd4167f0b8e9c0224998d3573acd48e22
qfdk <qfdk@vip.qq.com>
2017-01-10 00:10:22 +0100

    [Fix] 跨域问题

--- server/server.js
+++ server/server.js
@@ -20,6 +20,19 @@ server.listen(3000)
 console.log('Server on 3000; http://localhost:3000')
 
 // -----------------index-----------------
+//解决跨域
+app.all('*',function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT, POST');
+  if (req.method == 'OPTIONS') {
+    res.send(200); /让options请求快速返回/
+  }
+  else {
+    next();
+  }
+});
+
 app.get('/', function (req, res) {
   res.render('index')
 })
