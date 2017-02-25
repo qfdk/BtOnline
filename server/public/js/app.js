e449ce37e7364998250c80d2b8ffa4dd8063adb8
qfdk <qfdk@vip.qq.com>
2017-02-25 21:08:32 +0100

    [Fix] 修复逻辑错误

--- server/public/js/app.js
+++ server/public/js/app.js
@@ -1,5 +1,5 @@
 var client = new WebTorrent()
-var socket = io.connect("http://" + window.location['hostname'] + ":3000");
+var socket = io.connect('http://' + window.location['hostname'] + ':3000')
 
 DragDrop('body', function (files) {
   client.seed(files, function (torrent) {
@@ -10,92 +10,53 @@ DragDrop('body', function (files) {
 })
 
 client.on('error', function (err) {
-  console.error('ERROR: ' + err.message)
+  log(err.message)
 })
 
 $('#download').on('click', function (e) {
   e.preventDefault()
+  $('.log').html('')
+  log('正在添加下载任务 .. ')
   var torrentId = document.querySelector('form input[name=torrentId]').value
   // var url = "http://" + window.location['hostname'] + ":3000/download?url=" + torrentId
   socket.emit('download', { torrentId: torrentId })
-
 })
 
-socket.on('showInfo', function (data) {
+socket.on('showDownloadInfo', function (data) {
+  $('.log').html('')
   var json = JSON.parse(data)
-  if (json.isDownloading==='true') {
-    var tmp = (json.progress * 100).toFixed(1);
+  console.log(json)
+  if (json.isDownloading) {
+    $('.log').html('正在下载 ...')
+    var tmp = (json.progress * 100).toFixed(1)
     $('.progress-bar').attr('aria-valuenow', tmp)
     $('.progress-bar').attr('style', 'width:' + tmp + '%')
     $('.progress-bar').html(tmp + '%')
     $('#speed').html(json.downSpeed + '/S')
   } else {
+    console.log('im ok')
     $('.progress-bar').attr('aria-valuenow', 100)
     $('.progress-bar').attr('style', 'width:100%')
-    $('.progress-bar').html( '100% 下载完成')
-    $('#speed').html('访问http://' + window.location['hostname'] + ':8888进行查看')
+    $('.progress-bar').html('100% 下载完成')
+    $('#speed').html('访问 http://' + window.location['hostname'] + ':8888 进行查看')
+    $('.log').html('下载完成.')
   }
-
-
 })
 
-
-$('#online').on('click', function (e) {
-  e.preventDefault()
-  var torrentId = document.querySelector('form input[name=torrentId]').value
-  log('Adding ' + torrentId)
-  client.add(torrentId, onTorrent)
-})
-
-function onTorrent(torrent) {
-  log('Got torrent metadata!')
-  log(
-    'Torrent info hash: ' + torrent.infoHash + ' ' +
-    '<a href="' + torrent.magnetURI + '" target="_blank">[Magnet URI]</a> ' +
-    '<a href="' + torrent.torrentFileBlobURL + '" target="_blank" download="' + torrent.name + '.torrent">[Download .torrent]</a>'
-  )
-
-  // Print out progress every 5 seconds
-  var interval = setInterval(function () {
-    var tmp = (torrent.progress * 100).toFixed(1);
-    $('.progress-bar').attr('aria-valuenow', tmp)
-    $('.progress-bar').attr('style', 'width:' + tmp + '%')
-    $('.progress-bar').html(tmp + ' %')
-    $('#speed').html(prettyBytes(torrent.downloadSpeed) + '/S')
-    // log('Progress: ' + (torrent.progress * 100).toFixed(1) + '%')
-  }, 5000)
-
-  torrent.on('done', function () {
-    log('Progress: 100%')
-    clearInterval(interval)
-  })
-
-  // Render all files into to the page
-  torrent.files.forEach(function (file) {
-    file.appendTo('.log')
-    log('(Blob URLs only work if the file is loaded from a server. "http//localhost" works. "file://" does not.)')
-    file.getBlobURL(function (err, url) {
-      if (err) return log(err.message)
-      log('File done.')
-      log('<a href="' + url + '">Download full file: ' + file.name + '</a>')
-    })
-  })
-}
-
-function log(str) {
+function log (str) {
   var p = document.createElement('p')
   p.innerHTML = str
-  var para = document.createElement("p");
-  para.appendChild(p);
-  var element = document.querySelector('.log');
-  element.insertBefore(para, element.firstChild);
+  var para = document.createElement('p')
+  para.appendChild(p)
+  var element = document.querySelector('.log')
+  element.insertBefore(para, element.firstChild)
 }
 
 /**
  * 转换并显示下载速度
  * show the downloadSpeed for human
  */
-function prettyBytes(num) {
+function prettyBytes (num) {
   var exponent, unit, neg = num < 0, units = ['B', 'kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
   if (neg) num = -num
   if (num < 1) return (neg ? '-' : '') + num + ' B'
@@ -103,4 +64,45 @@ function prettyBytes(num) {
   num = Number((num / Math.pow(1000, exponent)).toFixed(2))
   unit = units[exponent]
   return (neg ? '-' : '') + num + ' ' + unit
+}
+
+$('#online').on('click', function (e) {
+  e.preventDefault()
+  $('.log').html('')
+  log('正在添加任务 .. ')
+  var torrentId = document.querySelector('form input[name=torrentId]').value
+  client.add(torrentId, onTorrent)
+})
+
+function onTorrent (torrent) {
+    // Print out progress every 5 seconds
+  var interval = setInterval(function () {
+    var tmp = (torrent.progress * 100).toFixed(1)
+    $('.progress-bar').attr('aria-valuenow', tmp)
+    $('.progress-bar').attr('style', 'width:' + tmp + '%')
+    $('.progress-bar').html(tmp + ' %')
+    $('#speed').html(prettyBytes(torrent.downloadSpeed) + '/S')
+    // log('Progress: ' + (torrent.progress * 100).toFixed(1) + '%')
+  }, 5000)
+
+  log(
+    'Torrent info hash: ' + torrent.infoHash + ' ' +
+    '<a href="' + torrent.magnetURI + '" target="_blank">[Magnet URI]</a> ' +
+    '<a href="' + torrent.torrentFileBlobURL + '" target="_blank" download="' + torrent.name + '.torrent">[Download .torrent]</a>'
+  )
+
+  torrent.on('done', function () {
+    clearInterval(interval)
+  })
+
+  // Render all files into to the page
+  torrent.files.forEach(function (file) {
+    // file.appendTo('.log')
+    log(file.name)
+    file.getBlobURL(function (err, url) {
+      if (err) return log(err.message)
+      log('File done.')
+      log('<a href="' + url + '">Download full file: ' + file.name + '</a>')
+    })
+  })
 }
--- server/server.js
+++ server/server.js
@@ -7,14 +7,11 @@ var io = require('socket.io')(server)
 var path = require('path')
 var favicon = require('serve-favicon')
 var WebTorrent = require('webtorrent')
-var sleep = require('sleep')
 var client = new WebTorrent()
 var dirPort = config.dirPort
 var listTorrents = []
-var sockets = []; // Liste des sockets client
-
+var sockets = [] // Liste des sockets client
 var isDownloading = true
-
 // ------------express-------------------
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
@@ -23,103 +20,100 @@ app.use(require('express').static(path.join(__dirname, 'public')))
 server.listen(3000)
 console.log('Server on 3000; http://localhost:3000')
 
-//解决跨域
+// 解决跨域
 app.all('*', function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT, POST');
-  if (req.method == 'OPTIONS') {
-    /*让options请求快速返回*/
-    res.send(200);
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild')
+  res.header('Access-Control-Allow-Methods', 'GET,PUT, POST')
+  if (req.method === 'OPTIONS') {
+    // 让options请求快速返回
+    res.send(200)
+  } else {
+    next()
   }
-  else {
-    next();
-  }
-});
+})
 
 app.get('/', function (req, res) {
   res.render('index')
 })
 
 io.sockets.on('connection', function (socket) {
-
   if (sockets.indexOf(socket) === -1) {
-    console.log(socket.id + ' connection');
-    sockets.push(socket);
+    console.log(socket.id + ' connection')
+    sockets.push(socket)
   }
 
   socket.on('download', function (data) {
     var torrentId = data.torrentId
-    if (('undefined' !== torrentId) && !isExsitedTorrentID(torrentId)) {
+    if ((torrentId !== 'undefined') && !isExsitedTorrentID(torrentId)) {
       listTorrents.push(torrentId)
       /**
        * 开始下载
        */
       client.add(torrentId, config, function (torrent) {
-        var files = torrent.files;
+        var files = torrent.files
         files.forEach(function (file) {
           setInterval(function () {
             var result = {}
-            result['isDownloading'] = true
+            if (isDownloading) {
+              result['isDownloading'] = true
+            } else {
+              result['isDownloading'] = false
+            }
             result['file'] = file.name
             result['downSpeed'] = prettyBytes(torrent.downloadSpeed)
             result['progress'] = torrent.progress
             result['timeRemaining'] = torrent.timeRemaining
-            io.sockets.emit('showInfo', JSON.stringify(result))
+            io.sockets.emit('showDownloadInfo', JSON.stringify(result))
           }, 500)
-
-        }, this);
+        }, this)
 
         torrent.on('done', function () {
-          console.log('torrent finished downloading');
-          isDownloading = false
-          torrent.files.forEach(function (file) {
-            console.log(file.name + ' 完成');
-            var server = torrent.createServer()
-            server.listen(dirPort)
+          console.log('种子下载完成')
+          torrent.files.forEach(function (file, index) {
+            console.log(file.name + '[' + index + '] 完成')
           })
+          // var server = torrent.createServer()
+          // server.listen(dirPort)
+          isDownloading = false
           var result = {}
           result['isDownloading'] = false
           result['downSpeed'] = prettyBytes(torrent.downloadSpeed)
           result['progress'] = torrent.progress
           result['timeRemaining'] = torrent.timeRemaining
-          io.sockets.emit('showInfo', JSON.stringify(result))
+          io.sockets.emit('showDownloadInfo', JSON.stringify(result))
         })
         // server.close()
         // client.destroy()
       })
-    } else {
-      console.log('已经下载完成');
-      var server = torrent.createServer()
-      server.listen(dirPort)
     }
-  });
+  })
 
   socket.on('disconnect', function (o) {
-    var indexSocket = sockets.indexOf(socket);
+    var indexSocket = sockets.indexOf(socket)
     if (indexSocket !== -1) {
       sockets.splice(indexSocket, 1)
-      console.log(socket.id + '离开');
+      console.log(socket.id + '离开')
     }
-  });
+  })
 })
 
 /**
  * 每个Torrent文件应该独立
  * id of torrentId must be uniq
  */
-function isExsitedTorrentID(torrentId) {
+function isExsitedTorrentID (torrentId) {
   if (listTorrents.indexOf(torrentId) !== -1) {
     return true
   }
-  return false;
+  return false
 }
 
 /**
  * 转换并显示下载速度
  * show the downloadSpeed for human
  */
-function prettyBytes(num) {
+function prettyBytes (num) {
   var exponent, unit, neg = num < 0, units = ['B', 'kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
   if (neg) num = -num
   if (num < 1) return (neg ? '-' : '') + num + ' B'
